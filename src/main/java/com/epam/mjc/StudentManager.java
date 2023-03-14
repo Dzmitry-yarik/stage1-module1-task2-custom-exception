4d9d0f42987f032aeb4d9df62ef09157c65c778e
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-14 21:58:48 +0300

    Update StudentManager.java

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -1,21 +1,32 @@
 package com.epam.mjc;
 
 
+class MyException extends IllegalArgumentException {
+    MyException(long s) {
+        System.err.println("Could not find student with ID " + s);
+    }
+}
+
 public class StudentManager {
+    private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    public Student find(long studentID) {
+        for (long i = 0; i < IDs.length-1; i++) {
+            if (studentID > 11 || studentID < 1) {
+                throw new MyException(studentID);
+            }
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
-  }
-
-  public static void main(String[] args) {
-    StudentManager manager = new StudentManager();
-
-    for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+        }
+        return Student.getValueOf(studentID);
     }
 
-  }
+    public static void main(String[] args) {
+        StudentManager manager = new StudentManager();
+
+        for (int i = 0; i < IDs.length-1; i++) {
+            Student student = manager.find(IDs[i]);
+            System.out.println("Student name " + student.getName());
+        }
+    }
+
 }
